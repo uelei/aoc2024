e04c2a5942750c4dbc5ce30440451bdfee161099
Wesley Werneck <github@uelei.com>
2024-12-08 18:47:11 -0300

    🌱 Update day01

--- day01/part1.py
+++ day01/part1.py
@@ -2,18 +2,65 @@ from __future__ import annotations
 
 import argparse
 import os.path
+import re
+from collections import defaultdict
+from typing import List
 
 import pytest
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), "input")
 
 
+LIN_REG = re.compile(r"(\d+)   (\d+)")
+
+
+class SolutionDay01:
+    l1: List[int] = []
+    l2: List[int] = []
+
+    def __init__(self, data):
+        self.data = data
+        self.parse_line()
+
+    def parse_line(self):
+        for line in self.data:
+            match = re.match(LIN_REG, line)
+
+            if match:
+                x, y = map(int, match.groups())
+
+                self.l1.append(x)
+                self.l2.append(y)
+
+            if not match:
+                print("no match for line ", line)
+
+    def part1(self):
+        self.parse_line()
+        total_distance = 0
+        ll1 = sorted(self.l1)
+        ll2 = sorted(self.l2)
+
+        for l1, l2 in zip(ll1, ll2):
+            total_distance += abs(l1 - l2)
+        return total_distance
+
+    def part2(self):
+        total_distance = 0
+        total_keys = defaultdict(int)
+
+        for k in self.l2:
+            total_keys[k] += 1
+
+        for k in self.l1:
+            total_distance += k * total_keys.get(k, 0)
+        return total_distance
+
+
 def compute(s: str) -> int:
-    lines = s.splitlines()
-    for line in lines:
-        pass
-    # TODO: implement solution here!
-    return 0
+    solution = SolutionDay01(s.splitlines())
+
+    return solution.part1()
 
 
 INPUT_S = """\
